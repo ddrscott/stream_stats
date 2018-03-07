8d948d02142e9fd5821b53a5b7968bee7af515b5
Scott Pierce <scott.pierce@centro.net>
2018-03-07 04:29:10 -0600

    Render stats in thread

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io::{self, BufRead, BufReader, BufWriter, stdin, Write};
 use std::string::{String};
 use std::time::{Duration, Instant};
+use std::thread::{self, sleep};
 
-const CAP : usize = 1024 * 1024;
+static CAP : usize = 1024 * 1024;
+static UPDATE_INTERVAL_MS : u64 = 500;
+
+static mut NUM_LINES : u64 = 0;
+static mut NUM_BYTES : u64 = 0;
 
 fn main() {
     let stdin = stdin();
@@ -10,27 +15,29 @@ fn main() {
     let mut reader = BufReader::with_capacity(CAP, stdin.lock());
     let mut writer = BufWriter::new(stdout.lock());
     let mut buffer = String::new();
-    let mut num_lines = 0;
-    let mut num_bytes = 0;
 
-    let started = Instant::now();
-    let mut last = Instant::now();
+    let started : Instant = Instant::now();
+
+    thread::spawn(move || {
+        loop {
+            sleep(Duration::from_millis(UPDATE_INTERVAL_MS));
+            unsafe { render(started, NUM_LINES, NUM_BYTES) }
+        }
+    });
 
     while reader.read_line(&mut buffer).unwrap() > 0 {
         writer.write(buffer.as_bytes()).unwrap();
-        num_lines += 1;
-        num_bytes += buffer.len();
-        buffer.clear();
-        if last.elapsed() > Duration::from_millis(789) {
-            render(&started, &num_lines, &num_bytes);
-            last = Instant::now();
+        unsafe {
+            NUM_LINES += 1;
+            NUM_BYTES += buffer.len() as u64;
         }
+        buffer.clear();
     }
-    render(&started, &num_lines, &num_bytes);
+    unsafe { render(started, NUM_LINES, NUM_BYTES) }
     eprint!("\n")
 }
 
-fn render(started: &Instant, num_lines : &usize, num_bytes: &usize) {
+fn render(started : Instant, num_lines : u64, num_bytes : u64) {
     let elapsed = started.elapsed();
     let seconds = elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9;
     if seconds == 0.0 {
@@ -38,8 +45,7 @@ fn render(started: &Instant, num_lines : &usize, num_bytes: &usize) {
     }
     let kb = num_bytes / 1024;
     let kb_per_sec : f64 = kb as f64 / 1024.0 / seconds;
-    let lines_per_sec : f64 = *num_lines as f64 / seconds;
-
+    let lines_per_sec : f64 = num_lines as f64 / seconds;
     eprint!("\x1B[1G\x1B[2K {:.1} sec | {} kb [ {:.1} kb/sec ] | {} lines [ {:.0} lines/sec ]",
               seconds, kb, kb_per_sec, num_lines, lines_per_sec);
 }

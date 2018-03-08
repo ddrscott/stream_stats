6ea1891dc65c60f41ca777c2b69504dba495d8b4
Scott Pierce <scott.pierce@centro.net>
2018-03-08 09:28:45 -0600

    Fixed number format

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,9 @@ use std::time::{Duration, Instant};
 use std::thread::{self, sleep};
 
 static READ_BUF_SIZE : usize = 1024 * 1024;
-static UPDATE_INTERVAL_MS : u64 = 500;
+static UPDATE_INTERVAL_MS : u64 = 100;
 static OUTPUT: &'static str = "/dev/tty";
 
-// Any data from `stdin` is passed to `stdout`.
 fn main() {
     let stdin = stdin();
     let stdout = io::stdout();
@@ -24,15 +23,13 @@ fn main() {
     let started : Instant = Instant::now();
     let output = Arc::new(Mutex::new(OpenOptions::new().write(true).append(true).open(OUTPUT).unwrap()));
 
-    {
-        let (num_lines, num_bytes, output) = (num_lines.clone(), num_bytes.clone(), output.clone());
-        thread::spawn(move || {
-            loop {
-                sleep(Duration::from_millis(UPDATE_INTERVAL_MS));
-                render(&mut output.lock().unwrap(), &started, num_lines.load(Ordering::Relaxed), num_bytes.load(Ordering::Relaxed));
-            }
-        });
-    }
+    let (clone_lines, clone_bytes, clone_output) = (num_lines.clone(), num_bytes.clone(), output.clone());
+    thread::spawn(move || {
+        loop {
+            sleep(Duration::from_millis(UPDATE_INTERVAL_MS));
+            render(&mut clone_output.lock().unwrap(), &started, clone_lines.load(Ordering::Relaxed), clone_bytes.load(Ordering::Relaxed));
+        }
+    });
 
     while reader.read_line(&mut buffer).unwrap() > 0 {
         writer.write(buffer.as_bytes()).unwrap();
@@ -46,22 +43,19 @@ fn main() {
 
 fn render(output: &mut File, started: &Instant, num_lines: usize, num_bytes: usize) {
     let elapsed = started.elapsed();
-    // let seconds : f64 = elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9;
-    let millis : f64 = (elapsed.as_secs() * 1000 + (elapsed.subsec_nanos() as u64 / 1000000 as u64)) as f64;
-    if millis == 0.0 {
+    let seconds : f64 = elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9;
+    if seconds == 0.0 {
         return;
     }
-    let kb = num_bytes / 1024 as usize;
-    let kb_per_millis = kb / (millis as usize);
-    let lines_per_millis = num_lines / millis as usize;
+    let kb = num_bytes as f64 / 1024 as f64;
+    let kb_per_sec = kb / seconds;
+    let lines_per_sec = num_lines as f64 / seconds;
     output.write_all(
         format!(
-            "\x1B[1G\x1B[2K {:.1} sec | {} kb [ {:.1} kb/s ] | {} lines [ {:.0} ln/s ]",
-            millis as f64 / 1000.0 as f64,
-            kb,
-            kb_per_millis as f64 / 1000.0 as f64,
-            num_lines,
-            lines_per_millis as f64 / 1000.0 as f64
+            "\x1B[1G\x1B[2K {:.1} sec | {:.0} kb [ {:.1}/s ] | {} lines [ {:.0}/s ]",
+            seconds,
+            kb, kb_per_sec,
+            num_lines, lines_per_sec
             ).as_bytes()
-        ) .unwrap();
+        ).unwrap();
 }

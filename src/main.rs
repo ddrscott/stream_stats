b83183dca162d8f5a320465f8d43bec1eb7bba98
Scott Pierce <scott.pierce@centro.net>
2018-03-09 03:50:06 -0600

    Moved some let statements and rename variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env};
 use std::fs::{File, OpenOptions};
-use std::io::{self, BufRead, BufReader, BufWriter, stdin, Write};
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::string::{String};
 use std::sync::{Arc, Mutex};
 use std::time::{Duration};
@@ -11,42 +11,43 @@ use stats::{Stats};
 
 static READ_BUF_SIZE : usize = 1024 * 1024;
 static UPDATE_INTERVAL_MS : u64 = 100;
-static OUTPUT : &str = "/dev/tty";
+static OUTPUT_TTY : &str = "/dev/tty";
 
 fn main() {
-    let stdout = io::stdout();
-    let mut reader = BufReader::with_capacity(READ_BUF_SIZE, file_or_stdin());
-    let mut writer = BufWriter::new(stdout.lock());
-    let mut buffer = String::new();
-
     let stats = Arc::new(Stats::new());
-    let output = Arc::new(Mutex::new(OpenOptions::new().write(true).append(true).open(OUTPUT).unwrap()));
+    let tty = Arc::new(Mutex::new(OpenOptions::new().write(true).append(true).open(OUTPUT_TTY).unwrap()));
 
     // Setup a thread to render stats periodically.
     let stats_clone = stats.clone();
-    let output_clone = output.clone();
+    let output_clone = tty.clone();
     thread::spawn(move || {
         loop {
             sleep(Duration::from_millis(UPDATE_INTERVAL_MS));
             write!(&mut output_clone.lock().unwrap(), "{}", &stats_clone)
-                .expect("Could not write to output!");
+                .expect("Could not write to tty!");
         }
     });
+
+    let mut reader = BufReader::with_capacity(READ_BUF_SIZE, file_or_stdin());
+    let mut writer = BufWriter::new(io::stdout());
+    let mut buffer = String::new();
 
     while reader.read_line(&mut buffer).unwrap() > 0 {
         writer.write(buffer.as_bytes()).unwrap();
         stats.add(&buffer);
         buffer.clear();
     }
-    writeln!(output.lock().unwrap(), "{}", &stats)
-        .expect("Could not write to output!");
+
+    // write file stats
+    writeln!(tty.lock().unwrap(), "{}", &stats)
+        .expect("Could not write to tty!");
 }
 
 /// Thanks: https://www.reddit.com/r/rust/comments/32rjdd/reading_from_a_file_or_stdin_based_on_command/
 fn file_or_stdin() -> Box<io::Read> {
     let path = env::args().nth(1).unwrap_or(String::from("-"));
     match path.as_ref() {
-        "-" => Box::new(stdin()),
+        "-" => Box::new(io::stdin()),
         _   => Box::new(File::open(path).unwrap())
     }
 }
